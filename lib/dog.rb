02fac5eed3381fb2be0cbe1787c534aa9a0e79c4
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-23 05:06:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Dog
+  #WRITE METHOD
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  #READ METHOD
+  def name 
+    @name
+  end
+  
+  #WRITE METHOD
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  #READ METHOD
+  def breed
+    @breed
+  end
+  
+end
