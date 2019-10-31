a3a0a13d610e2e21a1bd939a32cc1fa366dc1d37
Lenny Lee <lennylee91@gmail.com>
2019-10-31 22:42:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person
+  
+  attr_accessor :name, :job
+  
+  
+  
+end
+
+
+    #OR
+    
+  
+#   def name=(name)
+#     @name = name
+#   end
+  
+#   def name
+#     @name
+#   end
+  
+#   def job=(job)
+#     @job = job
+#   end
+  
+#   def job
+#     @job
+    
+#   end
+# end
+
