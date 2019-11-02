f065a2f7282c69653248547f6bd8e349ebbea40b
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-02 11:51:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  def name(dog_name)
+    @name = dog_name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(new_dog_name) 
+    @name = new_dog_name 
+  end 
+  
+  def breed(dog_breed) 
+    @breed = dog_breed 
+  end  
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(new_breed) 
+    @breed = new_breed 
+  end 
+end 
+
